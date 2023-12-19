29754848b18ed6b4ea65116b3e049d726dd225bb
CodrinCarpovici <gabriel_9729@yahoo.co.uk>
2023-12-19 16:40:33 +0000

    added basic api call function on input change for testing the response

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// Api key
+const apiKey = "d7b53c91bdd2abe33da074c99abbbe36";
+
+// Variables
+let lat, lon;
+
+// On input change, modify the cityName stored in the variable
+$("#search-input").on("change", function () {
+  const cityName = $(this).val().trim();
+  // City Query URL
+  const cityQueryURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+  // Fetching data using the city name
+  fetch(cityQueryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      // Extracting the lan and lon from the response
+      lat = data.coord.lat;
+      lon = data.coord.lon;
+
+      // Constructing the API call using the lan and lon values
+      const queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+      // Fectching the data using the URL
+      fetch(queryURL)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        });
+    });
+});
